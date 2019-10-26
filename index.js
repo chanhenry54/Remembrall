f156ed5b18ba033060ea77f6c897a15a0acda698
Henry Chan <hechan@ucsd.edu>
2019-10-26 16:26:59 -0700

    Set up basics for Discord bot

---
+++ index.js
@@ -0,0 +1,25 @@
+// startup declaration
+const Discord = require('discord.js');
+const fs = require('fs');
+require('dotenv').config();
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// load events from /events folder
+fs.readdir('./events/', (err, files) => {
+    if (err) { return console.error(err); }
+    files.forEach(file => {
+        if (!file.endsWith('.js')) { return; }
+        const event = require(`./events/${file}`);
+        let eventName = file.split('.')[0];
+
+        client.on(eventName, event.bind(null, client));
+        delete require.cache[require.resolve(`./events/${file}`)];
+    });
+});
+
+client.login(process.env.BOT_TOKEN);
