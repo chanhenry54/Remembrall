d4576807fdfe7b83864830e3bf4197417e6bdf3c
Henry Chan <hechan@ucsd.edu>
2019-10-26 22:00:58 -0700

    Separated parsing into functions.js

--- events/message.js
+++ events/message.js
@@ -1,3 +1,5 @@
+const util = require('../functions.js');
+
 module.exports = async (client, msg) => {
     // only get messages from announcements
     if (msg.channel.name !== 'announcements') { return; }
@@ -5,14 +7,5 @@ module.exports = async (client, msg) => {
     // ignore bot messages
     if (msg.author.bot) { return; }
 
-    // get initial str array
-    let eventStrArr = msg.toString().trim().split(' @ ');
-    if (eventStrArr.length !== 2) { return; }
-
-    // get event name
-    let eventName = eventStrArr[0];
-
-    // start parsing event place, date, time
-    let eventData = eventStrArr[1].split(', ');
-    if (eventData.length !== 3) { return; }
+    util.parseEvent(msg.toString());
 };
---
+++ functions.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    parseEvent(string) {
+        // get initial str array
+        let eventStrArr = string.toString().trim().split(' @ ');
+        if (eventStrArr.length !== 2) { return; }
+
+        // get event name
+        let eventName = eventStrArr[0];
+
+        // start parsing event place, date, time
+        let eventData = eventStrArr[1].split(', ');
+        if (eventData.length !== 3) { return; }
+    }
+};
