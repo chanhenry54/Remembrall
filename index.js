4fe11ee406da314b0e96593cffbb2e0816a9a8e6
Gaby Hourany <gchouran@ucsd.edu>
2019-10-26 22:47:09 -0700

    function to add event to google calendar

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const readline = require('readline');
 const {google} = require('googleapis');
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/calendar.events'];
+const SCOPES = ['https://www.googleapis.com/auth/calendar'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
@@ -36,7 +36,7 @@ const TOKEN_PATH = 'token.json';
 fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Calendar API.
-  authorize(JSON.parse(content), listEvents);
+  authorize(JSON.parse(content), addEvent);
 });
 
 /**
@@ -89,11 +89,24 @@ function getAccessToken(oAuth2Client, callback) {
   });
 }
 
+var event = {
+    'summary': 'kmsIMMEDIATELY',
+    'location': '2329 Navarro Dr., Claremont, CA 91711',
+    'start': {
+      'dateTime': '2019-10-27T09:00:00-07:00',
+      'timeZone': 'America/Los_Angeles',
+    },
+    'end': {
+      'dateTime': '2019-10-29T17:00:00-07:00',
+      'timeZone': 'America/Los_Angeles',
+    },
+    };
+
 /**
- * Lists the next 10 events on the user's primary calendar.
+ * add the event to the users google calendar
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-function listEvents(auth) {
+function addEvent(auth) {
   const calendar = google.calendar({version: 'v3', auth});
   calendar.events.list({
     calendarId: 'primary',
@@ -101,49 +114,7 @@ function listEvents(auth) {
     maxResults: 10,
     singleEvents: true,
     orderBy: 'startTime',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const events = res.data.items;
-    if (events.length) {
-      console.log('Upcoming 10 events:');
-      events.map((event, i) => {
-        const start = event.start.dateTime || event.start.date;
-        console.log(`${start} - ${event.summary}`);
-      });
-    } else {
-      console.log('No upcoming events found.');
-    }
-  });
-}
-
-var event = {
-    'summary': 'Google I/O 2015',
-    'location': '800 Howard St., San Francisco, CA 94103',
-    'description': 'A chance to hear more about Google\'s developer products.',
-    'start': {
-      'dateTime': '2015-05-28T09:00:00-07:00',
-      'timeZone': 'America/Los_Angeles',
-    },
-    'end': {
-      'dateTime': '2015-05-28T17:00:00-07:00',
-      'timeZone': 'America/Los_Angeles',
-    },
-    'recurrence': [
-      'RRULE:FREQ=DAILY;COUNT=2'
-    ],
-    'attendees': [
-      {'email': 'lpage@example.com'},
-      {'email': 'sbrin@example.com'},
-    ],
-    'reminders': {
-      'useDefault': false,
-      'overrides': [
-        {'method': 'email', 'minutes': 24 * 60},
-        {'method': 'popup', 'minutes': 10},
-      ],
-    },
-  };
-  
+  }),
   calendar.events.insert({
     auth: auth,
     calendarId: 'primary',
@@ -154,7 +125,8 @@ var event = {
       return;
     }
     console.log('Event created: %s', event.htmlLink);
-  });
+  }); 
+}
 
 client.login(process.env.BOT_TOKEN);
 
