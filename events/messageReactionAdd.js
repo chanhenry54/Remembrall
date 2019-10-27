b6364e23af0f8b8c89da0ea8644e99fbec5d632b
Henry Chan <hechan@ucsd.edu>
2019-10-26 22:46:49 -0700

    Reaction works properly

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,5 +1,7 @@
 module.exports = async (client, reaction, user) => {
+    if (user.bot) { return; }
     if (reaction.message.channel.name !== 'events') { return; }
-    if (reaction.emoji !== '📆') { return; }
-    user.send('ur mom gay')
+    if (reaction.emoji.toString() !== '📆') { return; }
+
+    user.send('ur mom gay');
 };
